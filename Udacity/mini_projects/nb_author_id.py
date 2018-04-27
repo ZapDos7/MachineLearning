5747fb7755577d936e454b6b7de4503bd22cb99b
Ioanna Zapalidi <ZapDos7@users.noreply.github.com>
2018-04-27 03:56:48 +0300

    #4
    Folder of mini projects of Udacity's course -- CAUTION, it requires additional data from:
    https://github.com/udacity/ud120-projects

---
+++ Udacity/mini_projects/nb_author_id.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+#########################################################
+### your code goes here ###
+
+### create classifier
+clf=GaussianNB()
+
+t0 = time()
+
+### fit the classifier on the training features and labels
+clf.fit(features_train, labels_train)
+
+print "training time:", round(time()-t0, 3), "s"
+
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+accuracy = metrics.accuracy_score(pred, labels_test)
+print accuracy
+#########################################################
+
+
